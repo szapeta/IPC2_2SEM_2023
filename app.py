3cabc78239385beeb8b0ce8dc41c0e1d312a76ad
szapeta <2375894860101@ingenieria.usac.edu.gt>
2023-07-27 08:54:14 -0600

    se realiza el ejemplo de la semana2 en python

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 #comentario
+
+numero = 10
+
+textoResultado = "Resultado: "
+
+numero2 = 2
+
+textRandom = "Random"
+
+union = str(numero2) +textRandom
+
+union2 = textRandom + str(numero2)
+
+print(union)
+print(union2)
+
